56db3410389bbde809594f592bf6223e247c7ea1
Michael Willett <mikerwillett@gmail.com>
2022-09-05 15:11:54 -0500

    added python script that will handle the plotting and data manipulation

---
+++ taxi.py
@@ -0,0 +1,5 @@
+# TODO: use argparse so that invoking this python script
+#       with the same args as those given to the taxify.go
+#       will grab the right CSV
+with open("./output/income=1000000_steps=100.csv") as f:
+    pass
