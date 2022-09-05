14339da71bcf532e3dca2ebafb5208b22715523f
Michael Willett <mikerwillett@gmail.com>
2022-09-05 15:24:40 -0500

    got python script running with argparse, dynamic filename creation, and opening a csv

--- taxi.py
+++ taxi.py
@@ -1,5 +1,28 @@
-# TODO: use argparse so that invoking this python script
-#       with the same args as those given to the taxify.go
-#       will grab the right CSV
-with open("./output/income=1000000_steps=100.csv") as f:
-    pass
+import argparse
+
+
+def main(args: list) -> None:
+    with open(get_filepath(args.income, args.steps), "r") as f:
+        print(f.read())
+
+
+def get_filepath(income: int, steps: int) -> str:
+    return f"./output/csv/income={income}_steps={steps}.csv"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-income",
+        type=int,
+        required=True,
+        help="income (type <int>), digits only, used in the CSV",
+    )
+    parser.add_argument(
+        "-steps",
+        type=int,
+        required=True,
+        help="number of steps (type <int>) used in the CSV",
+    )
+    args = parser.parse_args()
+    main(args)
