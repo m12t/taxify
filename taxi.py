887385b456de023bbf8e8be4fab83e83a701a130
Michael Willett <mikerwillett@gmail.com>
2022-09-05 18:01:01 -0500

    added requirements.txt

--- taxi.py
+++ taxi.py
@@ -9,6 +9,7 @@ A Python script for manipulating the CSV data returned by `taxify.go`.
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegFileWriter
 
 
 def main(args: list) -> None:
@@ -17,8 +18,15 @@ def main(args: list) -> None:
 
 
 def plot(df: pd.DataFrame) -> None:
-    for i in range(1, 53):
-        plt.plot(df["income"], df.iloc[:, i])
+    for i in range(1, df.shape[0]):
+        for j in range(1, df.shape[1]):
+            plt.plot(df["income"].iloc[:i], df.iloc[:i, j])
+        plt.xlim([0, df["income"].iloc[-1]])
+        plt.ylim([0, 0.38])
+        plt.title(df["income"].iloc[i])
+        plt.xlabel("Income")
+        plt.ylabel("Effective tax rate")
+        # plt.savefig(f"./output/plots/demo/{i}.png")
     plt.show()
 
 
