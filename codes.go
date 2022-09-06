a9675e7087aed17a965160c705629b514d369653
Michael Willett <mikerwillett@gmail.com>
2022-09-06 12:58:38 -0500

    added tax codes func for Alabama

---
+++ codes.go
@@ -0,0 +1,60 @@
+// A store for all tax codes encoded in functions
+package codes
+
+import (
+	"math"
+)
+
+// Try to do 5 states per day.
+
+func taxEngine(income *float64, brackets *[]int, rates *[]float64) float64 {
+	tax := 0.0
+	numBrackets := len(*brackets)
+	for i, bracket := range *brackets {
+		if i == numBrackets-1 {
+			tax += math.Max(0, (*income)-float64(bracket)) * (*rates)[i]
+		} else {
+			tax += math.Min(float64((*brackets)[i+1]-bracket), math.Max(0, (*income)-float64(bracket))) * (*rates)[i]
+		}
+	}
+	return tax
+}
+
+/* --------------------- state codes --------------------- */
+
+// Resources:
+// https://itep.sfo2.digitaloceanspaces.com/pb51fedinc.pdf
+func calcAlabamaTax(income, capitalGains, dividends *float64,
+	federalTax, numDependents int, mfj bool) (int, float64) {
+	tax, taxableIncome := 0.0, (*income)
+	taxableIncome += *capitalGains // gains are taxed as ordinary income
+	taxableIncome += *dividends    // gains are taxed as ordinary income
+	grossIncome := taxableIncome   // capture gross income now
+	brackets := []int{0, 500, 3000}
+	rates := []float64{0.02, 0.03, 0.05}
+	dependentExemption := 1000
+	standardDeduction := 2500
+	personalExemption := 1500
+	if mfj {
+		brackets = []int{0, 1000, 6000}
+		standardDeduction = 7500
+		personalExemption = 3000
+	}
+
+	// deductions
+	taxableIncome -= float64(numDependents * dependentExemption)
+	taxableIncome -= float64(standardDeduction)
+	taxableIncome -= float64(personalExemption)
+	taxableIncome -= float64(federalTax) // can deduct 100% of federal tax paid from taxableIncome
+
+	taxableIncome = math.Max(0, taxableIncome) // assert taxableIncome >= 0
+	tax += taxEngine(&taxableIncome, &brackets, &rates)
+
+	return int(tax), tax / grossIncome
+}
+
+// No income tax of any kind
+func calcAlaskaTax(income, capitalGains, dividends *float64,
+	federalTax, numDependents int, mfj bool) (int, float64) {
+	return 0, 0.0
+}
