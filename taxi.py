00422ece0b4e5a25115c5c9cc51aeed3bc9ce7fe
Michael Willett <mikerwillett@gmail.com>
2022-09-05 20:48:20 -0500

    refactored data and added single and join brackets and rates for all 50 + DC

--- taxi.py
+++ taxi.py
@@ -15,19 +15,44 @@ from matplotlib.animation import FuncAnimation, FFMpegFileWriter
 def main(args: list) -> None:
     df = pd.read_csv(get_filepath(args.income, args.steps))
     plot(df)
+    # animate(df, top=7)
 
 
-def plot(df: pd.DataFrame) -> None:
+def plot(df: pd.DataFrame, top: int = 53) -> None:
+    top = max(0, min(df.shape[1], top))
     for i in range(1, df.shape[0]):
-        for j in range(1, df.shape[1]):
+        for j in range(1, top):
             plt.plot(df["income"].iloc[:i], df.iloc[:i, j])
-        plt.xlim([0, df["income"].iloc[-1]])
-        plt.ylim([0, 0.38])
-        plt.title(df["income"].iloc[i])
-        plt.xlabel("Income")
-        plt.ylabel("Effective tax rate")
-        # plt.savefig(f"./output/plots/demo/{i}.png")
+    plt.xlim([0, df["income"].iloc[-1]])
+    plt.ylim([0, 0.38])
+    plt.title(df["income"].iloc[i])
+    plt.xlabel("Income")
+    plt.ylabel("Effective tax rate")
+    # plt.savefig(f"./output/plots/demo/{i}.png")
     plt.show()
+
+
+def animate(df: pd.DataFrame, top: int = 10) -> None:
+    top = max(0, min(df.shape[1], top))
+    plt, ax = plt.subplots()
+
+    plt.xlim([0, df["income"].iloc[-1]])
+    plt.ylim([0, 0.38])
+    plt.title(df["income"].iloc[i])
+    plt.xlabel("Income")
+    plt.ylabel("Effective tax rate")
+
+    ax.plot(df["income"].iloc[:i], df.iloc[:i, j])
+
+    plt.show()
+
+
+def add_data(plt: object, df: pd.DataFrame) -> object:
+    plt.set_ydata(np.sin(x + i / 50))  # update the data.
+    return plt
+
+
+ani = animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50)
 
 
 def get_filepath(income: int, steps: int) -> str:
